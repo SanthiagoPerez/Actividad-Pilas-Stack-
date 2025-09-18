5aa0642bee06704a5066e019f5afaf2a70bb22ed
GabyyCastro15 <gabyycastroo77@gmail.com>
2025-09-18 14:29:23 -0500

    Creacion de las 2 pilas, ingresar los datos por la consola y añadir datos a la pila

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Stack;
+import java.util.Scanner;
+
 public class Main {
+
+    static Stack<String> stack1 = new Stack<>();//creación del stack1
+    static Stack<String> stack2 = new Stack<>();//creación del stack2
+
+    Scanner entrada = new Scanner(System.in);
+    String consola = entrada.nextLine();
+    //Función que ingresa los datos de la consola al stack1
+    public static void ingresarPush() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingrese el Texto a guardar (Push): ");
+        String texto = entrada.nextLine();
+        stack1.push(texto);//lo añade
+    }
+
     public static void main(String[] args) {
+
+
 
     }
 }
